429b69f8ffb4901dfcff7631a558db8c808f7760
huayuenh <huayuenh@ie.ibm.com>
2026-03-06 16:13:05 +0000

    chore: remove insights

--- integrations/variables.tf
+++ integrations/variables.tf
@@ -128,17 +128,6 @@ variable "privateworker_credentials_secret_ref" {
 variable "authorization_policy_creation" {
 }
 
-variable "enable_insights" {
-}
-variable "link_to_doi_toolchain" {
-  description = "Enable a link to a DevOpsInsights instance in another toolchain, true or false"
-  type        = bool
-  default     = false
-}
-
-variable "doi_toolchain_id" {
-}
-
 variable "enable_artifactory" {
 }
 
